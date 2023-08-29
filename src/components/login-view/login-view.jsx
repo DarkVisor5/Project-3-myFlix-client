1e146f08c4524b816b837c3bb0f847910cfd81c9
DarkVisor5 <79510764+DarkVisor5@users.noreply.github.com>
2023-08-29 16:04:36 +0200

    3.5complete

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,60 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 
-esport const LoginView = ({onLoggedIn}) => {
-    const [username, setUsername]= useState("");
-    const [password, setPassword]= useState("");
+export const LoginView = ({ onLoggedIn }) => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-      
-        const data = {
-          username: username,
-          password: password
-        };
-      
-        fetch("https://testmovieapi.onrender.com/login", {
-          method: "POST",
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify(data)
-        })
-        .then(response => response.json())
-        .then(data => {
-          if(data.token) {
-            onLoggedIn(username);
-          } else {
-            alert("Login failed");
-          }
-        })
-        .catch(error => {
-          console.error("Error:", error);
-          alert("An error occurred while logging in.");
-        });
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-    return(
-        <form onSubmit={handleSubmit}>
-            <label>
-                Username:
-                <input
-                    type="text"
-                    value={username}
-                    onChange={(e)=>setUsername(e.target.value)}
-                />
-                
-            </label>
-            <label>
-                Password:
-                <input
-                    type="password"
-                    value={password}
-                    onChange={(e)=>setPassword(e.target.value)}
-                />
-            </label>
-            <button type="submit">
-                Submit
-            </button>
-        </form>
-    );
+    // Form validation
+    if (!username || !password) {
+      alert("All fields are required!");
+      return;
+    }
+
+    const data = {
+      username: username,
+      password: password,
+    };
+
+    // Fetch for logging in
+    fetch("https://testmovieapi.onrender.com/login", {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    })
+    .then(response => response.json())
+    .then(data => {
+      if (data.token) {
+        localStorage.setItem('jwt', data.token);  // Storing JWT token
+        onLoggedIn(username);
+      } else {
+        alert("Login failed");
+      }
+    })
+    .catch(error => {
+      console.error("Error:", error);
+      alert("An error occurred while logging in.");
+    });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Username:
+        <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+      </label>
+      <label>
+        Password:
+        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+      </label>
+      <button type="submit">Submit</button>
+    </form>
+  );
 };
-
